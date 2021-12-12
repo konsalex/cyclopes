d5316bc0d9971778841e14db0918658ec7e37608
konsalex <konsalexee@gmail.com>
2021-12-12 21:41:01 +0100

    :bookmark: Check npm publish

--- build.js
+++ build.js
@@ -6,12 +6,18 @@ const process = require("process");
 let ls;
 
 // Run command depending on the OS
-if (os.type() === "Linux") ls = spawn("echo", ["linux"]);
+if (os.type() === "Linux")
+  ls = spawn("./dist/cyclopes_linux_amd64/cyclopes", [
+    process.argv.slice(2).join(" "),
+  ]);
 else if (os.type() === "Darwin")
   ls = spawn("./dist/cyclopes_darwin_amd64/cyclopes", [
     process.argv.slice(2).join(" "),
   ]);
-else if (os.type() === "Windows_NT") ls = spawn("echo", ["windows"]);
+else if (os.type() === "Windows_NT")
+  ls = spawn("./dist/cyclopes_windows_amd64/cyclopes", [
+    process.argv.slice(2).join(" "),
+  ]);
 else throw new Error("Unsupported OS found: " + os.type());
 
 ls.stdout.on("data", (data) => {
